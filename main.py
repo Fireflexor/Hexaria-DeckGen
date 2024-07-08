6f0698520ecc18d68143dbbef4ae116cd4b517a8
Fireflexor <88890834+Fireflexor@users.noreply.github.com>
2024-07-08 21:04:25 +0100

    Update main.py
    i actually did fix it the first time

--- main.py
+++ main.py
@@ -149,11 +149,9 @@ def getObtainableCards():
     
     for card in wikiCards:
         rarity = getRarity(card)
-        print(rarity)
         if rarity == None:
             print(f"{card} does not have an assigned rarity")
         else:
-            print(f"{card} is {rarity} rarity")
             cardList[rarity].append(card)
 
 
